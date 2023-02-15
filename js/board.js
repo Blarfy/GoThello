87dbbd1f090f9eff25e4bca2f3c3fedf3306df61
Alex <mcbuzzer@gmail.com>
2023-02-15 14:16:54 -0700

    Board more stuff

--- js/board.js
+++ js/board.js
@@ -5,4 +5,28 @@ class Board {
             this.grid[i] = new Array(8);
         }
     }
+
+    clearBoard() {
+        for (i = 0; i < this.grid.length; i++) {
+            for (j = 0; j < this.grid[i].length; j++) {
+                this.grid[i][j] = null;
+            }
+        }
+    }
+
+    placePiece(piece, x, y) {
+        this.grid[x][y] = piece;
+    }
+
+    removePiece(x, y) {
+        this.grid[x][y] = null;
+    }
+
+    getPiece(x, y) {
+        return this.grid[x][y];
+    }
+
+    getGrid() {
+        return this.grid;
+    }
 }
