ad1ea815483afe87a7fbc01ee26158d77dcf6bef
Alex <mcbuzzer@gmail.com>
2023-02-15 14:15:41 -0700

    board constructor made

---
+++ js/board.js
@@ -0,0 +1,8 @@
+class Board {
+    constructor() {
+        this.grid = new Array(8);
+        for (i = 0; i < this.grid.length; i++) {
+            this.grid[i] = new Array(8);
+        }
+    }
+}
